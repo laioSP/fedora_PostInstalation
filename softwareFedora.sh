076885442b87ceed8bbdad311993915c708089d7
laioSP <60949953+laioSP@users.noreply.github.com>
2020-11-17 22:00:43 -0800

    Update softwareFedora.sh

--- softwareFedora.sh
+++ softwareFedora.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-
-
-yes | dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-yes | dnf install ffmpeg-libs
-yes | dnf config-manager --set-enabled fedora-cisco-openh264
-yes | dnf groupupdate Multimedia
+yes | dnf install gnome-tweak-tool
+yes | snap install snap-store
 yes | snap install sublime-text --classic
 yes | snap install sc-controller
 yes | snap install natron
 yes | snap install djv
-yes | yum install gparted
 yes | dnf install krita
 yes | dnf install vlc
-yes | dnf install steam
+dnf install -y steam
+wget https://zoom.us/client/latest/zoom_x86_64.rpm
+yes | dnf localinstall zoom_x86_64.rpm
 yes | dnf update
-
-
-
-
-
