601df1f4bd2831cada68beac64c5af3fcb44bf5f
laioSP <60949953+laioSP@users.noreply.github.com>
2020-12-09 02:50:46 -0800

    Update foundryIcons.sh

--- foundryIcons.sh
+++ foundryIcons.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-ICONPATH=$HOMEUSER_PATH/Pictures/foundryIcons
 HOMEUSER_PATH=/home/$USERNAME
-
+ICONPATH=$HOMEUSER_PATH/Pictures/foundryIcons
 
 NukeX=/usr/share/applications/NukeXIcon.desktop
 NukeStudio=/usr/share/applications/NukeStudio.desktop
@@ -11,17 +10,19 @@ NUKEVERSION=Nuke12.2v3
 
 touch $NukeX
 echo "[Desktop Entry]" > $NukeX
+echo "Terminal=true" >> $NukeX
 echo "Version=1.0" >> $NukeX
 echo "Type=Application" >> $NukeX
-echo "Exec=$HOMEUSER_PATH/$NUKEVERSION/$NUKELINK --NukeX" >> $NukeX
+echo "Exec=$HOMEUSER_PATH/$NUKEVERSION/$NUKELINK --NukeX %f" >> $NukeX
 echo "Name=NukeX" >> $NukeX
 echo "Icon=$ICONPATH/nukeX.png" >> $NukeX
 
 touch $NukeStudio
 echo "[Desktop Entry]" > $NukeStudio
+echo "Terminal=true" >> $NukeStudio
 echo "Version=1.0" >> $NukeStudio
 echo "Type=Application" >> $NukeStudio
-echo "Exec=$HOMEUSER_PATH/$NUKEVERSION/$NUKELINK --studio" >> $NukeStudio
+echo "Exec=$HOMEUSER_PATH/$NUKEVERSION/$NUKELINK --studio %f" >> $NukeStudio
 echo "Name=Nuke_Studio" >> $NukeStudio
 echo "Icon=$ICONPATH/nukeStudio.png" >> $NukeStudio
 
@@ -30,9 +31,10 @@ MARIVERSION=Mari4.6v4
 
 touch $Mari
 echo "[Desktop Entry]" > $Mari
+echo "Terminal=true" >> $Mari
 echo "Version=1.0" >> $Mari
 echo "Type=Application" >> $Mari
-echo "Exec=$HOMEUSER_PATH/$MARIVERSION/bin/$MARIVERSION" >> $Mari
+echo "Exec=$HOMEUSER_PATH/$MARIVERSION/bin/$MARIVERSION %f" >> $Mari
 echo "Name=Mari" >> $Mari
 echo "Icon=$ICONPATH/mari.png" >> $Mari
 
@@ -41,9 +43,10 @@ MODOVERSION=Modo14.2v1
 
 touch $Modo
 echo "[Desktop Entry]" > $Modo
+
 echo "Version=1.0" >> $Modo
 echo "Type=Application" >> $Modo
-echo "Exec=$HOMEUSER_PATH/$MODOVERSION/modo" >> $Modo
+echo "Exec=$HOMEUSER_PATH/$MODOVERSION/modo %f" >> $Modo
 echo "Name=Modo" >> $Modo
 echo "Icon=$ICONPATH/modo.png" >> $Modo
 
@@ -52,9 +55,10 @@ KATANAVERSION=Katana4.0v1
 
 touch $Katana
 echo "[Desktop Entry]" > $Katana
+echo "Terminal=true" >> $Katana
 echo "Version=1.0" >> $Katana
 echo "Type=Application" >> $Katana
-echo "Exec=/opt/$KATANAVERSION/katana" >> $Katana
 echo "Name=Katana" >> $Katana
 echo "Icon=$ICONPATH/katana.png" >> $Katana
+echo "Exec=/opt/$KATANAVERSION/katana %f" >> $Katana
 
