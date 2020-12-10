3c96ed158a7a7d21e853aecd62ad10e1d0dfd0ac
laioSP <60949953+laioSP@users.noreply.github.com>
2020-12-09 19:02:59 -0800

    Update softwareFedora.sh

--- softwareFedora.sh
+++ softwareFedora.sh
@@ -5,10 +5,22 @@ yes | snap install snap-store
 yes | snap install sublime-text --classic
 yes | snap install sc-controller
 yes | snap install natron
-yes | snap install djv
 yes | dnf install krita
-yes | dnf install vlc
+yes | dnf install discord
+dnf install -y vlc
 dnf install -y steam
-wget https://zoom.us/client/latest/zoom_x86_64.rpm
+if ls | grep zoom
+then
+	yes | dnf localinstall zoom_x86_64.rpm
+else
+	wget https://zoom.us/client/latest/zoom_x86_64.rpm
+	yes | dnf localinstall zoom_x86_64.rpm
+fi
 yes | dnf localinstall zoom_x86_64.rpm
+yes | dnf install php-pear php-devel gcc
+yes | dnf install ImageMagick ImageMagick-devel ImageMagick-perl
+yes | dnf install GraphicsMagick GraphicsMagick-devel GraphicsMagick-perl
 yes | dnf update
+
+
+
